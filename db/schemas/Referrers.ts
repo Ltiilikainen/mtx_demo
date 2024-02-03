7f1e9a3309a90fa4dd416af78cb9d5efd69dbf44
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-03 13:43:47 +0200

    Create schemas for initial mongodb collections

---
+++ db/schemas/Referrers.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+import Uploads from "./Uploads.js";
+
+const ReferrerSchema = new mongoose.Schema(
+  {
+    refName: {
+      type: String,
+      required: [true, "Referrer name is required"]
+    },
+    affiliation: {
+      type: String,
+      required: [true, "Referrer affiliation is required"]
+    },
+    image: {
+      type: Schema.Types.ObjectId,
+      ref: Uploads
+    },
+    content: {
+      type: String,
+      required: [true, "Reference text is required"]
+    }
+  },
+  {
+    query: {
+      populatePaths() {
+        return this.populate("image").exec();
+      }
+    }
+  }
+);
+
+export default mongoose.model("Referrers", ReferrerSchema);
