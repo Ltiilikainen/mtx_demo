a5f6f62256bf4582d58aa7a9ca007ca6a4fea6d9
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-04 01:02:16 +0200

    Add padding to navbar and navigation handling to logo and site title

--- src/client/Components/TopNav/NavBar.tsx
+++ src/client/Components/TopNav/NavBar.tsx
@@ -1,10 +1,22 @@
+import { useNavigate } from "react-router";
 import NavContainer from "./NavContainer";
 
 export default function NavBar() {
+  const navigate = useNavigate();
   return (
-    <div className="w-full h-[3.5rem] bg-slate-700 border-b[0.5px] border-b-slate-800 flex flex-row gap-4 text-white items-center">
-      <div className="ml-4">{"</Logo>"}</div>
-      <h2>Site Title</h2>
+    <div className="w-full h-[3.5rem] bg-slate-700 border-b[0.5px] border-b-slate-800 flex flex-row gap-4 text-white items-center px-4">
+      <div
+        className="ml-4 cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        {"<Logo/>"}
+      </div>
+      <h2
+        className="cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        Site Title
+      </h2>
       <NavContainer />
     </div>
   );
