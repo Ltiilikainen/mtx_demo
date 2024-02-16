a561703dad1f1859157c124a2842d3d37c69a2aa
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-17 00:34:05 +0200

    Create upload function

---
+++ src/server/upload.ts
@@ -0,0 +1,46 @@
+import multer from "multer";
+import path from "path";
+
+//create multer instance
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (
+      req,
+      _file,
+      callback: (error: Error | null, destination: string) => void
+    ) => {
+      callback(null, "./media/");
+    },
+    filename: (
+      req,
+      file,
+      callback: (error: Error | null, destination: string) => void
+    ) => {
+      const fileType = req.query.filetype;
+      const fileExtension = path.extname(file.originalname);
+      const filename =
+        path.parse(file.originalname).name + "-" + Date.now() + fileExtension;
+
+      req.body.path = `/media/${filename}`;
+
+      if (
+        fileType === "img" &&
+        !checkFiletype(fileExtension, [".jpg", ".jpeg", ".png", ".gif", ".svg"])
+      )
+        return callback(new Error("Invalid file type"), fileType);
+
+      callback(null, filename);
+    }
+  }),
+  limits: { fileSize: 3000000 }
+});
+
+//create multer upload function
+const img = upload.single("img");
+
+function checkFiletype(filetype: string, checklist: string[]) {
+  if (checklist.indexOf(filetype) < 0) return false;
+  else return true;
+}
+
+export default { img };
