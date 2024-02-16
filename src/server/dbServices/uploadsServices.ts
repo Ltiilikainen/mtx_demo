b60d4a40e6cb4d3b7c8f286901aa90f15161f66a
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-17 00:34:50 +0200

    Implement upload router and uploads-related queries

--- src/server/dbServices/uploadsServices.ts
+++ src/server/dbServices/uploadsServices.ts
@@ -6,4 +6,24 @@ function readUploads(query?: { [key: string]: unknown }) {
   else return Uploads.find();
 }
 
-export default { readUploads };
+function writeUpload(upload: UploadInput) {
+  return Uploads.create(upload);
+}
+
+async function updateUpload(id: string, info: { [key: string]: string }) {
+  const test = await Uploads.find({ _id: id });
+  if (test.length < 1) {
+    throw new Error("Referrer could not be found");
+  }
+  return Uploads.findOneAndUpdate({ _id: id }, info);
+}
+
+async function deleteUpload(id: string) {
+  const test = await Uploads.find({ _id: id });
+  if (test.length < 1) {
+    throw new Error("Referrer could not be found");
+  }
+  return Uploads.findOneAndDelete({ _id: id });
+}
+
+export default { readUploads, writeUpload, updateUpload, deleteUpload };
