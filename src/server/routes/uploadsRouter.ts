854d86fb41f56e9aba6d7e5e3db06937c3200d8f
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-17 00:45:24 +0200

    fix delete request address

--- src/server/routes/uploadsRouter.ts
+++ src/server/routes/uploadsRouter.ts
@@ -97,7 +97,7 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/api/referrers/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const id = req.params.id;
 
   try {
