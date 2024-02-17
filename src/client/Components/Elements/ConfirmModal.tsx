8bc4955749a29a90baa281bce710f19171f1e6c2
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-17 17:50:56 +0200

    Fix issue in which ConfirmModal did not display properly

--- src/client/Components/Elements/ConfirmModal.tsx
+++ src/client/Components/Elements/ConfirmModal.tsx
@@ -3,42 +3,43 @@ import Button from "./Button";
 type ConfirmModalProps = {
   refObject: React.MutableRefObject<HTMLDialogElement | null>;
   text: string;
-  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  subtitle?: string;
   confirmCallback: (parameter?: unknown) => void;
 };
 
 export default function ConfirmModal({
   refObject,
   text,
-  setIsOpen,
+  subtitle,
   confirmCallback
 }: ConfirmModalProps) {
   return (
     <dialog
       ref={refObject}
-      className="flex flex-col gap-4 p-4 rounded-sm border-slate-500 backdrop:bg-[#000] backdrop:opacity-50 border-[1px]"
+      className="relative bg-transparent h-[max-content] w-[max-content] max-h-[90%] mx-auto backdrop:bg-[#000] backdrop:opacity-50 border-[1px] border-slate-600"
     >
-      <h5>{text}</h5>
-      <div className="flex flex-row justify-between gap-2">
-        <Button
-          className="border-slate-600 border-[1px] px-5"
-          onClick={() => {
-            refObject.current?.close();
-            setIsOpen && setIsOpen(false);
-          }}
-        >
-          Cancel
-        </Button>
-        <Button
-          className="bg-slate-600 text-white px-5"
-          onClick={() => {
-            confirmCallback();
-            refObject.current?.close();
-            setIsOpen && setIsOpen(false);
-          }}
-        >
-          Confirm
-        </Button>
+      <div className="py-2 px-4 bg-white flex flex-col gap-2">
+        <h5 className="w-full text-center">{text}</h5>
+        {subtitle && <p className="mx-4">{subtitle}</p>}
+        <div className="flex flex-row justify-between gap-2 px-8">
+          <Button
+            className="border-slate-600 border-[1px] px-5"
+            onClick={() => {
+              refObject.current?.close();
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            className="bg-slate-600 text-white px-5"
+            onClick={() => {
+              confirmCallback();
+              refObject.current?.close();
+            }}
+          >
+            Confirm
+          </Button>
+        </div>
       </div>
     </dialog>
   );
