42373994a7fedb8c32f980e7e6a2bc135abeb6c7
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-21 11:40:45 +0200

    Rename frontend emailServices to contactService for clarity

--- src/client/Services/emailServices.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const baseURL = "http://localhost:3001/api";
-
-const sendContactForm = () => {
-  return axios.post(`${baseURL}/contact`).then((res) => res.data);
-};
-
-export default { sendContactForm };
