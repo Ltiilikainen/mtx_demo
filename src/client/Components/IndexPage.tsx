2970a71df14161ed27156563834e2d0a5e717c4d
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-22 16:33:09 +0200

    Add separate queries to index and news and references pages to facilitate limiting results

--- src/client/Components/IndexPage.tsx
+++ src/client/Components/IndexPage.tsx
@@ -1,12 +1,30 @@
 import NewsFeedElement from "./Elements/NewsFeedElement";
 import ReferenceList from "./Elements/ReferenceList";
 import PageContentWrapper from "./Elements/Wrappers/PageContentWrapper";
+import { useQuery } from "@tanstack/react-query";
+import newsFeedServices from "../Services/newsFeedServices";
+import referrersServices from "../Services/referrersServices";
 
 export default function IndexPage() {
+  const referenceQuery = useQuery({
+    queryKey: ["referrers"],
+    queryFn: () => {
+      return referrersServices.getAllReferrers(3);
+    }
+  });
+
+  const newsFeedQuery = useQuery({
+    queryKey: ["news", "index"],
+    queryFn: () => {
+      return newsFeedServices.getAllNews(6);
+    },
+    enabled: referenceQuery.isSuccess
+  });
+
   return (
     <PageContentWrapper classname="gap-4">
-      <ReferenceList limit={3} />
-      <NewsFeedElement limit={10} />
+      <ReferenceList referenceQuery={referenceQuery} />
+      <NewsFeedElement newsFeedQuery={newsFeedQuery} />
     </PageContentWrapper>
   );
 }
