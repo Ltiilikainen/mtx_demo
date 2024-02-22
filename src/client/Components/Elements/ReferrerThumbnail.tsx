579d948e3eaf0cdda10cce8b0861171cd80e9b91
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-22 15:41:27 +0200

    Fix referrer thumbnail display

--- src/client/Components/Elements/ReferrerThumbnail.tsx
+++ src/client/Components/Elements/ReferrerThumbnail.tsx
@@ -4,8 +4,8 @@ type ReferrerThumbProps = {
 
 export default function ReferrerThumbnail({ referrer }: ReferrerThumbProps) {
   return (
-    <div className="relative w-60 h-40 my-2">
-      <div className="flex  w-full justify-center">
+    <div className="w-60 h-40 my-4">
+      <div className="flex w-full justify-center">
         <div className="w-20 h-20 bg-slate-300 rounded-full overflow-hidden">
           {referrer.image && (
             <img
@@ -15,10 +15,10 @@ export default function ReferrerThumbnail({ referrer }: ReferrerThumbProps) {
           )}
         </div>
       </div>
-      <div className="absolute top-16 w-full text-center">
+      <div className="text-center w-full  -translate-y-5">
         <h5 className="text-shadow-white font-semibold">{referrer.refName}</h5>
         <p>{referrer.affiliation}</p>
-        <div className="text-center mx-auto bg-slate-300 border-[1px] border-slate-800 rounded-xl w-60">
+        <div className="text-center mx-auto px-4 py-2 bg-slate-300 border-[1px] border-slate-800 rounded-xl ">
           <p>
             {`"`}
             {referrer.content}
