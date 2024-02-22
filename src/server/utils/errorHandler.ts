b8fef35b8f669768315265f710ee49e1e4ee5cd5
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-02-22 15:14:49 +0200

    Call mongo disconnect in error handler

--- src/server/utils/errorHandler.ts
+++ src/server/utils/errorHandler.ts
@@ -1,11 +1,14 @@
 import { MongoError } from "mongodb";
 import errorLogger from "./logger";
+import { mongoDisconnect } from "../dbServices/mongoConnect";
 
 export default function handleError(e: unknown, callback: () => void) {
-  if (e instanceof MongoError) {
-    errorLogger.log("error", "Mongo error: " + e.message);
-  } else {
-    errorLogger.log("error", (e as Error).message);
-  }
-  callback();
+  mongoDisconnect().then(() => {
+    if (e instanceof MongoError) {
+      errorLogger.log("error", "Mongo error: " + e.message);
+    } else {
+      errorLogger.log("error", (e as Error).message);
+    }
+    callback();
+  });
 }
